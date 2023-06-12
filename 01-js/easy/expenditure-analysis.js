15be5b730a71be2d102cea64dc542a7d6aac6570
SPDiw <swaroopdiwakar007@gmail.com>
2023-06-12 17:13:50 +0530

    Update expenditure-analysis.js

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,53 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let res = []
+  for (let transaction of transactions) {
+    let i = 0
+    for (i = 0; i < res.length; i++) {
+        if (res[i].category === transaction.category) {
+            res[i].price += transaction.price
+            break
+        }
+    }
+    if (i == res.length) {
+        res.push({category: transaction.category, price: transaction.price})
+    }
+  }
+  return res
 }
+
+// Input
+
+// let transactions = [
+//     {
+//         category: 'item1',
+//         price: 400.
+//     },
+//     {
+//         category: 'item2',
+//         price: 500.
+//     },
+//     {
+//         category: 'item1',
+//         price: 200.
+//     },
+//     {
+//         category: 'item2',
+//         price: 100.
+//     },
+//     {
+//         category: 'item3',
+//         price: 900.
+//     },
+// ]
+
+// Output
+
+// [
+//     { category: 'item1', price: 600 },
+//     { category: 'item2', price: 600 },
+//     { category: 'item3', price: 900 }
+// ]
 
 module.exports = calculateTotalSpentByCategory;
